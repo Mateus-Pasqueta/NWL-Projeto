e44c7e4e7020129771e22054d82e8d88fa7dd649
Mateus-Pasqueta <Mateuspasquetap@outlook.com>
2024-02-20 23:20:29 -0300

    feat: adding total hits

--- script.js
+++ script.js
@@ -51,8 +51,12 @@ const perguntas = [
     },
 ];
 const quiz = document.querySelector('#quiz')
-
 const template = document.querySelector('template')
+
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal = document.querySelector('#acertos span')
+mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
 
 
 // loop 
@@ -63,9 +67,22 @@ for (const item of perguntas) {
     for (let resposta of item.respostas) {
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
+        dt.querySelector('input').setAttribute('name', 'pergunta-' + perguntas.indexOf(item))
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+        dt.querySelector('input').onchange = (event) => {
+            const estaCorreta = event.target.value == item.correta
 
+            corretas.delete(item)
+            if(estaCorreta){
+                corretas.add(item)
+            }
+            
+            mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        }
+
+        
+        
         quizItem.querySelector('dl').appendChild(dt)
-
     }
 
     quizItem.querySelector('dl dt').remove()
